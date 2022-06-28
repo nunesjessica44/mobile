5fed047712d26ca740b2ed1bee7d5728f57ce6ef
nunesjessica44 <nunesjessica44@outlook.com>
2022-06-28 01:13:56 -0300

    formulario de cadastro

--- nativeBase/forms/App.js
+++ nativeBase/forms/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import {
   Text,
-  Link,
-  HStack,
-  Center,
   Heading,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
   extendTheme,
+  FormControl,
   VStack,
+  Button,
+  Input,
   Box,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
@@ -27,59 +25,39 @@ export const theme = extendTheme({ config });
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <Heading size="lg">Formulário de cadastro</Heading>
+      <Box>
+        <BuildingAForm></BuildingAForm>
+      </Box> 
     </NativeBaseProvider>
   );
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+function BuildingAForm() {
+  const [formData, setData] = React.useState({});
+  return <VStack width="90%" mx="3" maxW="300px">
+      <FormControl isRequired>
+        <FormControl.Label _text={{bold: true}}>
+          Nome
+        </FormControl.Label>
+        <Input placeholder="digite o nome"/>
+        <FormControl.Label _text={{bold: true}}>
+          Email
+        </FormControl.Label>
+        <Input placeholder="digite o e-mail"/>
+      </FormControl>
+      <FormControl.Label _text={{bold: true}}>
+          Senha
+        </FormControl.Label>
+        <Input placeholder="digite a senha"/>
+        <FormControl.Label _text={{bold: true}}>
+          Confirmar senha
+        </FormControl.Label>
+        <Input placeholder="digite a senha novamente"/>
+
+        <Button small primary>
+          <Text>Enviar cadastro</Text>
+        </Button>
+    </VStack>;
 }
+
